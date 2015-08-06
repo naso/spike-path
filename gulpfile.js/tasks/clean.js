96bf120dadeafaf66606aa1e3e3308bb9f928431
Fábio Azevedo <fabio.azevedo@unit9.com>
2015-08-07 00:36:29 +0100

    clean

--- gulpfile.js/tasks/clean.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var gulp = require('gulp')
-var del = require('del')
-
-gulp.task('clean', function (done) {
-  del([global.path.dist + '/**/*.*', global.path.dist], done)
-})
--- gulpfile.js/tasks/reset.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var gulp = require('gulp')
-var del = require('del')
-
-gulp.task('reset', ['clean'], function (done) {
-  del([global.path.src + '/lib/jspm', 'node_modules', '.sass-cache'], done)
-})
